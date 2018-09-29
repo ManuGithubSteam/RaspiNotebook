8c6c2ff5b8433ec5f0315448b358ca354a99c22d
Manuel <manuelsoukup@googlemail.com>
2018-09-29 13:21:48 +0200

    initial commit
    all working, heart sensor needs coding but raw values are there

---
+++ pi/heart_pulse.py
@@ -0,0 +1,81 @@
+# Simple demo of reading each analog input from the ADS1x15 and printing it to
+# the screen.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Additional needed modules will be imported and configured
+import signal, sys, os
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+ 
+# Used variables will be initialized
+beatsPerMinute = 0
+isPeak = False
+result = False
+delayTime = 0.01
+maxValue = 0
+schwelle = 25
+beatTime = 0
+oldBeatTime = 0
+
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
+
+
+# Create an ADS1115 ADC (16-bit) instance.
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Or create an ADS1015 ADC (12-bit) instance.
+#adc = Adafruit_ADS1x15.ADS1015()
+
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
+
+
+
+
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
+# Main loop.
+values = [0]*4
+while True:
+    # Read all the ADC channel values in a list.
+    #
+    for i in range(1):
+        # Read the specified ADC channel using the previously set gain value.
+	values[1] = values[0]
+        values[i] = adc.read_adc(i, gain=1) 
+	values[3] = (values[0] - values[1]) *(values[0] - values[1])
+	if values[3] > 4000:
+		values[3] = 1
+	else:
+		values[3] = 0
+
+        #values[i] =values[i]  * values[i]
+# Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 12 or 16 bit signed integer value depending on the
+        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+    # Print the ADC values.
+    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.01)
